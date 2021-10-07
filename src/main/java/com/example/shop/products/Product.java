8cad6253dde968733fe87effb7bf8b40eaec211a
karolpetka <karolpetka@gmail.com>
2021-10-07 16:07:15 +0200

    Set name field in Product class to unique

--- src/main/java/com/example/shop/products/Product.java
+++ src/main/java/com/example/shop/products/Product.java
@@ -11,7 +11,7 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false, unique = true)
     private String name;
 
     @Column(name = "quantity", nullable = false)
