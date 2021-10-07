8fa65d653a41f0407e3552b999efa630de02f41b
karolpetka <karolpetka@gmail.com>
2021-10-07 14:09:15 +0200

    Initialize H2 database and modify http in WebSecurityConfig to allow H2 database to work

--- src/main/java/com/example/shop/security/WebSecurityConfig.java
+++ src/main/java/com/example/shop/security/WebSecurityConfig.java
@@ -12,6 +12,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().authorizeRequests().anyRequest().permitAll();
+        http.headers().frameOptions().disable().and().cors().and().csrf().disable().authorizeRequests().anyRequest().permitAll();
     }
 }
