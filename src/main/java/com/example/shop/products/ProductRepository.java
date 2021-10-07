efe05f09430bd7afaf787abae5f68dc2a1946451
karolpetka <karolpetka@gmail.com>
2021-10-07 15:53:32 +0200

    Extend ProductRepository with JpaRepository interface

--- src/main/java/com/example/shop/products/ProductRepository.java
+++ src/main/java/com/example/shop/products/ProductRepository.java
@@ -1,7 +1,8 @@
 package com.example.shop.products;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
