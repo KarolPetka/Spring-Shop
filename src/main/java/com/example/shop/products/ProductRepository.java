26d95e5c2e916d9878e1c0a1f745fa6020813ba4
karolpetka <karolpetka@gmail.com>
2021-10-07 15:43:22 +0200

    Injected ProductRepository in ProductService class

--- src/main/java/com/example/shop/products/ProductRepository.java
+++ src/main/java/com/example/shop/products/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.example.shop.products;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface ProductRepository {
 }
--- src/main/java/com/example/shop/products/ProductService.java
+++ src/main/java/com/example/shop/products/ProductService.java
@@ -1,7 +1,15 @@
 package com.example.shop.products;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 }
