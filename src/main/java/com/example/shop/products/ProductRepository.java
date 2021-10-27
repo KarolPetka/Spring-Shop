35cc3e2fccbcdc9db16323a01aedfbbb18f107e3
karolpetka <karolpetka@gmail.com>
2021-10-27 19:15:43 +0200

    Add deleteProductByName method in ProductRepository class

--- src/main/java/com/example/shop/products/ProductRepository.java
+++ src/main/java/com/example/shop/products/ProductRepository.java
@@ -1,8 +1,13 @@
 package com.example.shop.products;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("DELETE FROM Product p WHERE p.name = :name")
+    void deleteProductByName(@Param("name") String name);
 }
