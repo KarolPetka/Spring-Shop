a46520e8f4e4ef16acd426d814f9898b4a533160
karolpetka <karolpetka@gmail.com>
2021-10-27 16:47:27 +0200

    Implement HTTP POST request for product entity and implement FileStorageException

--- src/main/java/com/example/shop/products/ProductService.java
+++ src/main/java/com/example/shop/products/ProductService.java
@@ -1,14 +1,21 @@
 package com.example.shop.products;
 
+import com.example.shop.exception.FileStorageException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class ProductService {
 
+    protected final String targetLocation = "./uploads/products";
     private final ProductRepository productRepository;
 
     @Autowired
@@ -21,6 +28,19 @@ public class ProductService {
     }
 
     public void postProduct(String name, int quantity, String description, MultipartFile file) {
+        String fileName = null;
 
+        if (file != null) {
+            Random random = new Random();
+            fileName = StringUtils.cleanPath(random.nextInt(10000) + "-" + file.getOriginalFilename());
+            try {
+                Files.write(Paths.get(targetLocation).resolve(fileName), file.getBytes());
+            } catch (IOException ex) {
+                throw new FileStorageException("Could not store file " + fileName + ". Please try again!", ex);
+            }
+        }
+
+        Product product = new Product(name, quantity, description, targetLocation + "/" + fileName);
+        productRepository.save(product);
     }
 }
