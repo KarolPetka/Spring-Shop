94bd4e175e42392001041a76b124fcd4d0f1adf8
karolpetka <karolpetka@gmail.com>
2021-10-27 20:05:41 +0200

    Add findProductUsingName and deleteProductByName query method

--- src/main/java/com/example/shop/products/ProductRepository.java
+++ src/main/java/com/example/shop/products/ProductRepository.java
@@ -1,13 +1,20 @@
 package com.example.shop.products;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    @Query("SELECT p.name FROM Product p WHERE p.name = :name")
+    String findProductUsingName(@Param("name") String name);
+
+    @Transactional
+    @Modifying
     @Query("DELETE FROM Product p WHERE p.name = :name")
     void deleteProductByName(@Param("name") String name);
 }
