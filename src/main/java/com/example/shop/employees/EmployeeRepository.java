95b0b2d9882f8ee6de978480af3bbf22f6ae9535
karolpetka <karolpetka@gmail.com>
2021-10-28 14:19:40 +0200

    Injected EmployeeRepository in EmployeeService

--- src/main/java/com/example/shop/employees/EmployeeRepository.java
+++ src/main/java/com/example/shop/employees/EmployeeRepository.java
@@ -1,4 +1,7 @@
 package com.example.shop.employees;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface EmployeeRepository {
 }
--- src/main/java/com/example/shop/employees/EmployeeService.java
+++ src/main/java/com/example/shop/employees/EmployeeService.java
@@ -1,7 +1,15 @@
 package com.example.shop.employees;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
 }
