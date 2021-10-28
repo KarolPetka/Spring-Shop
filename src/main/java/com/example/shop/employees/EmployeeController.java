b9ec41c0c46785fe48e16b5dd7f5237a7c7114a2
karolpetka <karolpetka@gmail.com>
2021-10-28 14:18:15 +0200

    Injected EmployeeService in EmployeeController

--- src/main/java/com/example/shop/employees/EmployeeController.java
+++ src/main/java/com/example/shop/employees/EmployeeController.java
@@ -1,4 +1,18 @@
 package com.example.shop.employees;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/employees")
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
 }
