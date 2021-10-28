255497f41cbc8a8f6d6424e46faa762cc062d7cb
karolpetka <karolpetka@gmail.com>
2021-10-28 13:02:20 +0200

    Implement Employee entity class

--- src/main/java/com/example/shop/employees/Employee.java
+++ src/main/java/com/example/shop/employees/Employee.java
@@ -1,4 +1,70 @@
 package com.example.shop.employees;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
 public class Employee {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "surname", nullable = false)
+    private String surname;
+
+    @Column(name = "position", nullable = false)
+    private String position;
+
+    public Employee() {
+    }
+
+    public Employee(String name, String surname, String position) {
+        this.name = name;
+        this.surname = surname;
+        this.position = position;
+    }
+
+    public Employee(Long id, String name, String surname, String position) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.position = position;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
 }
