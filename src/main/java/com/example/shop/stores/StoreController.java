04bb7facee92e0056e3f03758155821184664217
karolpetka <karolpetka@gmail.com>
2021-10-29 12:25:21 +0200

    Inject StoreService class into StoreController class

--- src/main/java/com/example/shop/stores/StoreController.java
+++ src/main/java/com/example/shop/stores/StoreController.java
@@ -1,4 +1,17 @@
 package com.example.shop.stores;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/stores")
 public class StoreController {
+
+    private final StoreService storeService;
+
+    @Autowired
+    public StoreController(StoreService storeService) {
+        this.storeService = storeService;
+    }
 }
