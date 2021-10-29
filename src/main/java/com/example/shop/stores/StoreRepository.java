956023f224fd27df9a2153ee1b1cc54c8867a5ff
karolpetka <karolpetka@gmail.com>
2021-10-29 12:27:41 +0200

    Inject StoreRepository interface into StoreService class

--- src/main/java/com/example/shop/stores/StoreRepository.java
+++ src/main/java/com/example/shop/stores/StoreRepository.java
@@ -1,4 +1,7 @@
 package com.example.shop.stores;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface StoreRepository {
 }
--- src/main/java/com/example/shop/stores/StoreService.java
+++ src/main/java/com/example/shop/stores/StoreService.java
@@ -1,7 +1,15 @@
 package com.example.shop.stores;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StoreService {
+
+    private final StoreRepository storeRepository;
+
+    @Autowired
+    public StoreService(StoreRepository storeRepository) {
+        this.storeRepository = storeRepository;
+    }
 }
