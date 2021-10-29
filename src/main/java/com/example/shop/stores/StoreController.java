d5fb13e5c5efd094d776fc6796809b972c924ed9
karolpetka <karolpetka@gmail.com>
2021-10-29 14:17:33 +0200

    Implement HTTP GET request

--- src/main/java/com/example/shop/stores/StoreController.java
+++ src/main/java/com/example/shop/stores/StoreController.java
@@ -1,8 +1,11 @@
 package com.example.shop.stores;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/stores")
@@ -13,5 +16,10 @@ public class StoreController {
     @Autowired
     public StoreController(StoreService storeService) {
         this.storeService = storeService;
+    }
+
+    @GetMapping
+    public List<Store> getStores() {
+        return storeService.getStores();
     }
 }
--- src/main/java/com/example/shop/stores/StoreService.java
+++ src/main/java/com/example/shop/stores/StoreService.java
@@ -3,6 +3,8 @@ package com.example.shop.stores;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class StoreService {
 
@@ -11,5 +13,9 @@ public class StoreService {
     @Autowired
     public StoreService(StoreRepository storeRepository) {
         this.storeRepository = storeRepository;
+    }
+
+    public List<Store> getStores() {
+        return storeRepository.findAll();
     }
 }
