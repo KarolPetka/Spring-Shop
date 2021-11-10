6af16f28f4f9df0af515aa80d42a6b71723beef6
karolpetka <karolpetka@gmail.com>
2021-11-10 13:10:45 +0100

    add test cases that passes

--- src/test/java/com/example/shop/products/ProductRepositoryTest.java
+++ src/test/java/com/example/shop/products/ProductRepositoryTest.java
@@ -1,9 +1,13 @@
 package com.example.shop.products;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,18 +18,36 @@ public class ProductRepositoryTest {
     private ProductRepository repositoryTest;
 
     @BeforeEach
+    @AfterEach
     void setUp() {
         repositoryTest.deleteAll();
     }
 
-
     @Test
     void findProductUsingName() {
-        fail();
+        //given
+        Product product = new Product("Product Name", 1, null, null);
+        repositoryTest.save(product);
+
+        //when
+        String productName = repositoryTest.findProductUsingName(product.getName());
+
+        //then
+        assertEquals(productName, product.getName());
+        assertNotEquals("Not existing product", product.getName());
     }
 
     @Test
     void deleteProductByName() {
-        fail();
+        //given
+        Product product = new Product("Product Name", 1, null, null);
+        repositoryTest.save(product);
+
+        //when
+        repositoryTest.deleteProductByName(product.getName());
+        List<Product> listAfterDelete = repositoryTest.findAll();
+
+        //then
+        assertTrue(listAfterDelete.isEmpty());
     }
 }
