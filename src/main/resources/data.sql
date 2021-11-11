b945d5cade1cd300713d467494f04c070237199e
karolpetka <karolpetka@gmail.com>
2021-11-11 17:52:31 +0100

    refactor file name

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO product (name, quantity, description, file_location) VALUES ('Karo', 23, 'Best corn syrup You can get!', './uploads/products/1000-karo.jpg'), ('Doritos', 15, 'Only Doritos I can have is Doritos Days', ''), ('Pizza', 7, '', './uploads/products/1001-pizza.jpg');
+INSERT INTO product (name, quantity, description, file_location) VALUES ('Karo', 23, 'Best corn syrup You can get!', './uploads/products/karo.jpg'), ('Doritos', 15, 'Only Doritos I can have is Doritos Days', ''), ('Pizza', 7, '', './uploads/products/pizza.jpg');
 INSERT INTO employee (name, surname, position) VALUES ('Karo', 'Petka', 'CEO'), ('Rajon', 'Rondo', 'General Manager'), ('Derek', 'Carr', 'Manager');
 INSERT INTO store (location) VALUES ('Poland'), ('USA'), ('Japan');
 INSERT INTO users (username, password) VALUES ('admin', '$2a$12$UKyUw2kWhP4tO1tID0Wh1.bjEulv5qLtX5EFBxTnHp/7vfbjsgcxu')
