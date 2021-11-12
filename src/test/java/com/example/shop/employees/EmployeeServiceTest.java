89fda6998667872e22fceb2015e8eb66586f2519
karolpetka <karolpetka@gmail.com>
2021-11-12 21:16:59 +0100

    implement failing test cases

---
+++ src/test/java/com/example/shop/employees/EmployeeServiceTest.java
@@ -0,0 +1,28 @@
+package com.example.shop.employees;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getProducts() {
+        fail();
+    }
+
+    @Test
+    void postEmployee() {
+        fail();
+    }
+
+    @Test
+    void deleteStore() {
+        fail();
+    }
+}
