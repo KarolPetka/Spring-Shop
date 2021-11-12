4b0acded396146de3da8b2ec51bfcca6abf72875
karolpetka <karolpetka@gmail.com>
2021-11-12 21:11:42 +0100

    add test cases that passes

--- src/test/java/com/example/shop/stores/StoreServiceTest.java
+++ src/test/java/com/example/shop/stores/StoreServiceTest.java
@@ -1,14 +1,15 @@
 package com.example.shop.stores;
 
-import com.example.shop.products.ProductRepository;
-import com.example.shop.products.ProductService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class StoreServiceTest {
@@ -24,16 +25,38 @@ class StoreServiceTest {
 
     @Test
     void getStores() {
-        fail();
+        //when
+        serviceTest.getStores();
+
+        //then
+        verify(repositoryTest).findAll();
     }
 
     @Test
     void postStore() {
-        fail();
+        //given
+        Store store = new Store("USA");
+
+        //when
+        serviceTest.postStore(store.getLocation());
+
+        //then
+        ArgumentCaptor<Store> storeArgumentCaptor = ArgumentCaptor.forClass(Store.class);
+        verify(repositoryTest).save(storeArgumentCaptor.capture());
+        Store capturedStore = storeArgumentCaptor.getValue();
+
+        assertEquals(store.getLocation(), capturedStore.getLocation());
     }
 
     @Test
     void deleteStore() {
-        fail();
+        //given
+        Store store = new Store(1L, "USA");
+
+        //when
+        serviceTest.deleteStore(1L);
+
+        //then
+        verify(repositoryTest, times(1)).deleteById(store.getId());
     }
 }
