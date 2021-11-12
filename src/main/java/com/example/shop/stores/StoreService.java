ae5b8fb69a66860598fa4b79061124cb77cb9636
karolpetka <karolpetka@gmail.com>
2021-11-12 21:02:27 +0100

    remove unnecessary import

--- src/main/java/com/example/shop/stores/StoreService.java
+++ src/main/java/com/example/shop/stores/StoreService.java
@@ -1,6 +1,5 @@
 package com.example.shop.stores;
 
-import com.example.shop.exceptions.StoreNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
