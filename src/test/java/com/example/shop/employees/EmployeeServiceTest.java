65d6b4eb376ae99310a773cda871b011986d92bd
karolpetka <karolpetka@gmail.com>
2021-11-12 21:49:59 +0100

    add passing test cases

--- src/test/java/com/example/shop/employees/EmployeeServiceTest.java
+++ src/test/java/com/example/shop/employees/EmployeeServiceTest.java
@@ -2,27 +2,63 @@ package com.example.shop.employees;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 class EmployeeServiceTest {
+
+    @Mock
+    private EmployeeRepository repositoryTest;
+    private EmployeeService serviceTest;
 
     @BeforeEach
     void setUp() {
+        serviceTest = new EmployeeService(repositoryTest);
     }
 
     @Test
     void getProducts() {
-        fail();
+        //when
+        serviceTest.getProducts();
+
+        //then
+        verify(repositoryTest).findAll();
     }
 
     @Test
     void postEmployee() {
-        fail();
+        //given
+        Employee employee = new Employee("Karo", "Petka", "CEO");
+
+        //when
+        serviceTest.postEmployee(employee.getName(), employee.getSurname(), employee.getPosition());
+
+        //then
+        ArgumentCaptor<Employee> employeeArgumentCaptor = ArgumentCaptor.forClass(Employee.class);
+        verify(repositoryTest).save(employeeArgumentCaptor.capture());
+        Employee capturedEmployee = employeeArgumentCaptor.getValue();
+
+        assertEquals(employee.getName(), capturedEmployee.getName());
+        assertEquals(employee.getSurname(), capturedEmployee.getSurname());
+        assertEquals(employee.getPosition(), capturedEmployee.getPosition());
     }
 
     @Test
     void deleteStore() {
-        fail();
+        //given
+        Employee employee = new Employee(1L,"Karo", "Petka", "CEO");
+
+        //when
+        serviceTest.deleteStore(employee.getId());
+
+        //then
+        verify(repositoryTest, times(1)).deleteById(employee.getId());
     }
 }
